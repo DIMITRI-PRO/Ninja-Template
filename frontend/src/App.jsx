520acc554478984409c91b41a6dd8b9de2834bff
CHOUMMANIVONGDimitri <dimitrichoummanivong@gmail.com>
2023-05-07 23:04:59 +0200

    ✅ import Authcontext

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,10 @@
+import { Home } from "./pages/Home";
 import { AuthContextProvider } from "./context/AuthContext";
-import { AuthRouter } from "./router/Router";
 
 export const App = () => {
   return (
     <AuthContextProvider>
-      <AuthRouter />
+      <Home />
     </AuthContextProvider>
   );
 };
