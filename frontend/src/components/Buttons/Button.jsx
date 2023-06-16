cf520d9c04145f6cdeb018adc0d74a4dd4c569fb
CHOUMMANIVONGDimitri <dimitrichoummanivong@gmail.com>
2023-06-16 20:23:46 +0200

    ✅ update Button component : add id

--- frontend/src/components/Buttons/Button.jsx
+++ frontend/src/components/Buttons/Button.jsx
@@ -1,24 +1,33 @@
 import PropTypes from "prop-types";
 
-export const Button = ({ type, onClick, children }) => {
+export const Button = ({ id, name, icon, type, onClick, children }) => {
   return (
     <button
-      className={`ninja btn-${type || "basic"}`}
+      id={id || null}
+      className={`ninja btn-${name || type || "basic"}`}
       type={type || "button"}
       onClick={onClick || null}
+      style={{ display: "flex" }}
     >
+      {icon && <img src={icon} id={`${id}-icon`} alt="icon button" />}
       {children}
     </button>
   );
 };
 
 Button.propTypes = {
+  id: PropTypes.string,
+  name: PropTypes.string,
   type: PropTypes.string,
+  icon: PropTypes.node,
   onClick: PropTypes.func,
-  children: PropTypes.element,
+  children: PropTypes.node,
 };
 Button.defaultProps = {
+  id: null,
+  name: null,
   type: null,
+  icon: null,
   onClick: () => {},
   children: null,
 };
