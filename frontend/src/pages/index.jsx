3b400a1cc9ee4a38b8e20fd94c1b33fc0895767d
CHOUMMANIVONGDimitri <dimitrichoummanivong@gmail.com>
2023-06-17 00:09:26 +0200

    ✅ update : import/export Pages

---
+++ frontend/src/pages/index.jsx
@@ -0,0 +1,6 @@
+import { Home } from "./Public/Home";
+import { Login } from "./Public/Login";
+import { NotFound } from "./Public/NotFound";
+import { Register } from "./Public/Register";
+
+export { Home, Login, NotFound, Register };
