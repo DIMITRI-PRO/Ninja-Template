6b8a76c16ef57c3aa341ed54303ed33fadbf48d7
CHOUMMANIVONGDimitri <dimitrichoummanivong@gmail.com>
2023-06-21 22:00:42 +0200

    ✅ frontend : import component

--- frontend/src/components/NinjaComp.jsx
+++ frontend/src/components/NinjaComp.jsx
@@ -2,8 +2,19 @@ import { Forms } from "./Form/Form";
 import { Button } from "./Buttons/Button";
 import { LanguageSwitch } from "./Buttons/LanguageSwitch";
 import { BasicMenu } from "./Menus/BasicMenu";
+import { NotificationMessage } from "./Layers/NotificationMessage";
 import { BasicMenuLayer } from "./Layers/BasicMenuLayer";
+import { ProgressBar } from "./Progress/ProgressBar";
 
 const { Form, FormItem } = Forms;
 
-export { Form, FormItem, Button, BasicMenu, BasicMenuLayer, LanguageSwitch };
+export {
+  Form,
+  FormItem,
+  Button,
+  BasicMenu,
+  BasicMenuLayer,
+  LanguageSwitch,
+  ProgressBar,
+  NotificationMessage,
+};
